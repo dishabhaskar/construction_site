be70210cd6baa0db98a236eeac72c36a4edf2391
dakshinamurthy <dakshinamurthy13@gmail.com>
2020-12-08 23:01:52 +0530

    Cost Estimator 1.0

--- javascript/script.js
+++ javascript/script.js
@@ -2,7 +2,7 @@
 /*Code for Slide Show of cards */
 $('#recipeCarousel').carousel({
   interval: 10000
-})
+});
 
 $('.carousel .carousel-item').each(function(){
     var minPerSlide = 4;
@@ -22,3 +22,17 @@ $('.carousel .carousel-item').each(function(){
       }
 });
 /*Code for Sild Show ends*/
+/**Cost Estimator Code */
+var costForm=document.getElementById("cost-form");
+costForm.addEventListener('submit',function(event){
+  event.preventDefault();
+  var result=document.getElementById("result");
+  var cost=document.getElementById("cost");
+  var length=document.getElementById("length").value;
+  var breadth=document.getElementById("breadth").value;
+  var floor=document.getElementById("floor").value;
+  var costAmount=floor*length*breadth;
+  cost.innerHTML=costAmount;
+  result.style.display="block";
+});
+/**Cost Estimator Code End */
